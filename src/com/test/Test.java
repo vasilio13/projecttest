d69ef63b6617d8b30245df38e04a02cf3621a417
vasilio13 <vasilkhaliaukin@gmail.com>
2019-12-04 21:24:13 +0300

    work with files

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,7 +1,30 @@
 package com.test;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //FileInputStream fileInputStream = new FileInputStream("D://v/test.txt");
+        File file = new File("D://v/test.txt");
+        //String[] list = file.list();
+        if (file.exists()) {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, 200);
+                int read = 0;
+                do {
+                    read = bufferedInputStream.read();
+                    System.out.print((char) read);
+
+                } while (read != -1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
